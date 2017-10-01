1ac42265766e52c89a82a8fbb6cd6792623978be
Arthur N <arturkkos@gmail.com>
2017-10-01 16:54:17 +0500

    added: babel, changed: ES6 import & export

--- newbot.js
+++ newbot.js
@@ -1,48 +1,48 @@
-const Telegraf = require('telegraf');
-const chalk = require('chalk');
-const { Extra, Markup } = Telegraf;
-const config = require('./config');
-const utilities = require('./utilities');
+import Telegraf from 'telegraf';
+import chalk from 'chalk';
+import { Extra, Markup} from 'telegraf';
+import config from './config';
+import { randomAnswer, rateAnswer, logIn} from './utilities';
 const bot = new Telegraf(config.telegraf_token);
 
 console.log(chalk.blue(`   Bot started!
 `));
 
 bot.hears(/(рейт)|(rate)/i, ctx => {
-  ctx.reply(utilities.rateAnswer(), {
+  ctx.reply(rateAnswer(), {
     reply_to_message_id: ctx.message.message_id
   });
-  utilities.logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title);
+  logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title);
 });
 
 bot.hears(/(^тян)/i, ctx => {
   Math.random() >= 0.66 ? ctx.reply(`*ЭХХХХХХ, КАК ЖЕ ПЛОХО БЕЗ ТЯНОЧКИ*`, {
     reply_to_message_id: ctx.message.message_id,
-    parse_mode: 'Markdown'}) && utilities.logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title) : null
+    parse_mode: 'Markdown'}) && logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title) : null
 });
 
 bot.hears(/(^бот,)(\s)([\wа-яё].{1,})(или)(\s)([\wа-яё].{1,})([\?]{1}$)/im, ctx => {
   const arr = /(^бот,)(\s)([\wа-яё].{1,})(или)(\s)([\wа-яё].{1,})([\?]{1}$)/im.exec(ctx.message.text);
   Math.random() >= 0.5 ? ctx.reply(arr[6], {
-    reply_to_message_id: ctx.message.message_id}) && utilities.logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title) : ctx.reply(arr[3], {
-      reply_to_message_id: ctx.message.message_id}) && utilities.logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title)
+    reply_to_message_id: ctx.message.message_id}) && logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title) : ctx.reply(arr[3], {
+      reply_to_message_id: ctx.message.message_id}) && logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title)
 })
 
 bot.hears(/(^бот,)(\s)([\wа-яё].{1,})([\?]{1}$)/im, ctx => {
-  ctx.reply(utilities.randomAnswer(), {
+  ctx.reply(randomAnswer(), {
     reply_to_message_id: ctx.message.message_id
   });
-  utilities.logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title);
+  logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title);
 });
 
 bot.use((ctx) => {
   let check = ``;
   ctx.message.caption === undefined ? null : check = ctx.message.caption;
   if (/(рейт)|(rate)/i.test(check) === true) {
-    ctx.reply(utilities.rateAnswer(), {
+    ctx.reply(rateAnswer(), {
       reply_to_message_id: ctx.message.message_id
     });
-    utilities.logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title);
+    logIn(ctx.message.from.username, ctx.message.from.first_name, ctx.message.chat.title);
   }
 })
 
--- utilities.js
+++ utilities.js
@@ -1,23 +1,22 @@
-const chalk = require('chalk');
-const random = require('./db/random.json');
-const rate = require('./db/rate.json');
-var exports = module.exports = {};
+import chalk from 'chalk';
+import random from './db/random.json';
+import rate from './db/rate.json';
 
-exports.randomAnswer = () => { return random[Math.floor(Math.random() * random.length)] }
+export const randomAnswer = () => { return random[Math.floor(Math.random() * random.length)] }
 
-exports.rateAnswer = () => { return rate[Math.floor(Math.random() * rate.length)] }
+export const rateAnswer = () => { return rate[Math.floor(Math.random() * rate.length)] }
 
-exports.logIn = (username, firstName, chatTitle) => {
-  let date = new Date();
-  let mins = date.getMinutes();
-  let hours = date.getHours();
-  mins < 10 ? (mins = '0' + mins.toString()) : null;
-  hours < 10 ? (hours = '0' + hours.toString()) : null;
-  console.log(
-    `   ${chalk.blue(`${hours}:${mins}`)} ${chalk.green(
-      `Sent answer`
-    )} to ${chalk.green(`@${username} (${firstName})`)} in ${chalk.green(
-      chatTitle
-    )} !`
-  );
-};
+export const logIn = (username, firstName, chatTitle) => {
+    let date = new Date();
+    let mins = date.getMinutes();
+    let hours = date.getHours();
+    mins < 10 ? (mins = '0' + mins.toString()) : null;
+    hours < 10 ? (hours = '0' + hours.toString()) : null;
+    console.log(
+      `   ${chalk.blue(`${hours}:${mins}`)} ${chalk.green(
+        `Sent answer`
+      )} to ${chalk.green(`@${username} (${firstName})`)} in ${chalk.green(
+        chatTitle
+      )} !`
+    );
+  };
